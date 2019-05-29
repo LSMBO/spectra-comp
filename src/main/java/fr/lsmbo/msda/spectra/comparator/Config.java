c009fed23040ae13de7f520bc15607aa97f07d01
Romdhani <romdhani@live.fr>
2019-05-29 15:22:50 +0200

    [spectra-comparator]  add databases acess

---
+++ src/main/java/fr/lsmbo/msda/spectra/comparator/Config.java
@@ -0,0 +1,12 @@
+package fr.lsmbo.msda.spectra.comparator;
+
+/**
+ * Initialize spectra-comparator configurations
+ * 
+ * @author Aromdhani
+ *
+ */
+
+public class Config {
+
+}
---
+++ src/main/java/fr/lsmbo/msda/spectra/comparator/DatabaseAcess.java
@@ -0,0 +1,11 @@
+package fr.lsmbo.msda.spectra.comparator;
+
+/**
+ * Handle database connection for spectra-comparator
+ * 
+ * @author Aromdhani
+ *
+ */
+public class DatabaseAcess {
+
+}
