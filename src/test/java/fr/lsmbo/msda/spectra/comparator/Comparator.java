0ffc02215f06a725c759df69423e0706ea21b8af
Romdhani <romdhani@live.fr>
2019-05-29 12:35:18 +0200

    [spectra-comparator] initial commit

---
+++ src/test/java/fr/lsmbo/msda/spectra/comparator/Comparator.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package fr.lsmbo.msda.spectra.comparator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Aromdhani
+ *
+ */
+public class Comparator {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
